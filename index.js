b792982c793d2f9efe7939adc0069bbee3541f9e
Raman Khanna <89792896+ramannkhanna2@users.noreply.github.com>
2023-01-30 16:00:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/will', function (req, res) {
     res.send('{ "response": "Hello World" }');
 });
 app.get('/ready', function (req, res) {
-    res.send('{ "response": " Great!, Its working !!" }');
+    res.send('{ "response": " Hurray!!!, Its working !!" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
