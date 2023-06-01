fa1c5f56e17f53ba5398d765c3e899b8f27f48ea
Raman Khanna <89792896+ramannkhanna2@users.noreply.github.com>
2023-06-01 23:53:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/will', function (req, res) {
-    res.send('{ "response": "Hello World" }');
+    res.send('{ "response": "Hello All" }');
 });
 app.get('/ready', function (req, res) {
     res.send('{ "response": " Hurray!!!, Its working !!" }');
